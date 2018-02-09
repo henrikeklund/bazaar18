b2e37049b8a5eedb1bd29a861df53a49b0cc1f15
Henrik Eklund <henrikek@1x-193-157-215-252.uio.no>
2018-02-09 14:48:14 +0100

    Fixed mean function for complex numbers

--- ws_assert_try.py
+++ ws_assert_try.py
@@ -1,11 +1,14 @@
 #assert True==True
+#sdgds
 def mean(num_list):
     try:
-        return sum(num_list)/len(num_list)
+        mean = sum(num_list)/len(num_list)
+        if isinstance(mean, complex):
+            return NotImplemented
+        return mean
     except ZeroDivisionError as detail:
         msg='\nCannot compute with list length = 0'
         raise ZeroDivisionError(detail.__str__() + msg)
     except TypeError as detail:
         msg='/nPlease pass a list of numbers not strings'
         raise TypeError(detail.__str__() + msg)
-        
