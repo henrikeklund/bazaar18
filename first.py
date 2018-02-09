8b3967d1acf05f1c03011868b21deeb3ab0ecdd8
Henrik Eklund <henrikek@1x-193-157-215-252.uio.no>
2018-02-09 14:08:16 +0100

    Just to do tests

--- first.py
+++
@@ -1,6 +0,0 @@
-#assert True==True
-def mean(num_list):
-    if len(num_list)==0:
-        raise Exception('Cannot compute with list length = 0')
-    else:
-        return sum(num_list)/len(num_list)
