f8108ed5e56660a3d994896b89d683afcec47401
Henrik Eklund <henrikek@1x-193-157-215-252.uio.no>
2018-02-09 14:42:40 +0100

    again

--- test_1.py
+++ test_1.py
@@ -2,18 +2,25 @@ from ws_assert_try import mean
 
 def test_ints():
     num_list=[1,2,3,4,5]
-    abs=mean(num_list)
+    obs=mean(num_list)
     exp=3
-    assert abs==exp
+    assert obs==exp
     
 def test_double():
     num_list=[1,2,3,4]
-    abs=mean(num_list)
+    obs=mean(num_list)
     exp=2.5
-    assert abs==exp
+    assert obs==exp
     
 def test_long():
     big=100000000
-    abs=mean(range(1,big))
+    obs=mean(range(1,big))
     exp=big/2.0
-    assert abs==exp
+    assert obs==exp
+    
+def test_complex():
+    #testing for complex numbers
+    num_list=(2+3j, 3+4j, -32-2j)
+    obs=mean(num_list)
+    exp=NotImplemented
+    assert obs==exp
